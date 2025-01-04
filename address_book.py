65e49d1323e4fb6da7875f5c076742d87e6cd805
Maryna-Korbet <m22v86k@gmail.com>
2025-01-04 13:40:19 -0500

    update adress_book

--- address_book.py
+++ address_book.py
@@ -14,10 +14,10 @@ class AddressBook(UserDict):
         self.data[self.last_id] = record
     
     def find(self, name: str) -> Record | None:
-      for record in self.data.values():
-          if record.name.value == name:
-              return record
-      return None
+        for record in self.data.values():
+            if record.name.value == name:
+                return record
+        return None
 
     def delete(self, name: str):
         key = next((key for key in self.data.keys() if self.data[key].name.value == name), None)
@@ -28,28 +28,28 @@ class AddressBook(UserDict):
         print(f"Record {name} successfully deleted")
 
     def get_upcoming_birthdays(self):
-      result = []
-      current_date = datetime.today()
-      current_year = current_date.year
+        result = []
+        current_date = datetime.today()
+        current_year = current_date.year
 
-      for record in self.data.values():
-          if not record.birthday:
-              continue
+        for record in self.data.values():
+            if not record.birthday:
+                continue
 
-          # Замінюємо рік народження на поточний рік
-          birthdate = record.birthday.value.replace(year=current_year)
-          if birthdate < current_date:
-              birthdate = birthdate.replace(year=current_year + 1)
+            # Замінюємо рік народження на поточний рік
+            birthdate = record.birthday.value.replace(year=current_year)
+            if birthdate < current_date:
+                birthdate = birthdate.replace(year=current_year + 1)
 
-          days_until_birthday = (birthdate - current_date).days
-          if days_until_birthday <= 7:
-              congratulation_date = birthdate
-              if congratulation_date.weekday() in [5, 6]:  # Корекція для вихідних
-                  congratulation_date += timedelta(days=(7 - congratulation_date.weekday()))
+            days_until_birthday = (birthdate - current_date).days
+            if days_until_birthday <= 7:
+                congratulation_date = birthdate
+                if congratulation_date.weekday() in [5, 6]: 
+                    congratulation_date += timedelta(days=(7 - congratulation_date.weekday()))
 
-              result.append({
-                  "name": record.name.value,
-                  "congratulation_date": congratulation_date.strftime(DATE_FORMAT)  # Використання strftime
-              })
+                result.append({
+                    "name": record.name.value,
+                    "congratulation_date": congratulation_date.strftime(DATE_FORMAT) 
+                })
 
-          return result  
+            return result  
